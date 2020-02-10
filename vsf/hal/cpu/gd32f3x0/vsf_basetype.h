9e80353e7e282e5b310d984560300e6fcc8226ef
talpachen <talpachen@gmail.com>
2020-02-10 13:50:38 +0800

    remove old vsf source

--- vsf/hal/cpu/gd32f3x0/vsf_basetype.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __VSF_BASETYPE_H_INCLUDED__
-#define __VSF_BASETYPE_H_INCLUDED__
-
-// define the most efficient atom integer
-typedef int vsf_int_t;
-
-#endif	// __VSF_BASETYPE_H_INCLUDED__
