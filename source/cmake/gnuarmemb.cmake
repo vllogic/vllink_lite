3aaa876ea793a78f9fd19f81349b77a53a52ded4
talpachen <talpachen@gmail.com>
2020-02-24 11:36:59 +0800

    update cmake

--- source/cmake/gnuarmemb.cmake
+++ source/cmake/gnuarmemb.cmake
@@ -11,27 +11,34 @@ set(CMAKE_OBJCOPY arm-none-eabi-objcopy CACHE INTERNAL "objcopy")
 set(CMAKE_OBJDUMP arm-none-eabi-objdump CACHE INTERNAL "objdump")
 set(CMAKE_SIZE arm-none-eabi-size CACHE INTERNAL "size")
 
-set(CMAKE_C_FLAGS "-mthumb -mcpu=${VSFHAL_ARCH_NAME} -std=c99 -fno-builtin -fdata-sections -fms-extensions -ffunction-sections" CACHE INTERNAL "c compiler flags")
-set(CMAKE_CXX_FLAGS "-mthumb -mcpu=${VSFHAL_ARCH_NAME} -fno-builtin -fdata-sections -fms-extensions -ffunction-sections" CACHE INTERNAL "cxx compiler flags")
+set(CMAKE_C_FLAGS 
+  "-mthumb -mcpu=${VSFHAL_ARCH_NAME} -mfloat-abi=${ARM_FLOATING_ABI} ${ARM_FLOATING_HARDWARE} \
+    -std=c99 -Wall -fno-builtin -fdata-sections -fms-extensions -ffunction-sections"
+  CACHE INTERNAL "c compiler flags")
+set(CMAKE_CXX_FLAGS 
+  "-mthumb -mcpu=${VSFHAL_ARCH_NAME} -mfloat-abi=${ARM_FLOATING_ABI} ${ARM_FLOATING_HARDWARE} \
+    -Wall -fno-builtin -fdata-sections -fms-extensions -ffunction-sections"
+  CACHE INTERNAL "cxx compiler flags")
 set(CMAKE_ASM_FLAGS "-mthumb -mcpu=${VSFHAL_ARCH_NAME}" CACHE INTERNAL "asm compiler flags")
 
-set(CMAKE_C_FLAGS_DEBUG "-O0 -g -gstabs+" CACHE INTERNAL "c debug compiler flags")
-set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -gstabs+" CACHE INTERNAL "cxx debug compiler flags")
+# Optimization Level
+#   -O0       None
+#   -Os       Optimize for size
+#   -O1       Optimize
+#   -O2       Optimize more
+#   -O3       Optimize most
+#   -Og       Optimize for debug
+#   -Ofast    Optimize for speed
+set(CMAKE_C_FLAGS_DEBUG "-Og -g3 -gstabs+" CACHE INTERNAL "c debug compiler flags")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -g3 -gstabs+" CACHE INTERNAL "cxx debug compiler flags")
 set(CMAKE_ASM_FLAGS_DEBUG "-g -gstabs+" CACHE INTERNAL "asm debug compiler flags")
 
 set(CMAKE_C_FLAGS_RELEASE "-Os" CACHE INTERNAL "c release compiler flags")
 set(CMAKE_CXX_FLAGS_RELEASE "-Os" CACHE INTERNAL "cxx release compiler flags")
 set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "asm release compiler flags")
 
-set(CMAKE_C_LINKER_WRAPPER_FLAG "-Wl," CACHE INTERNAL "")
-set(CMAKE_CXX_LINKER_WRAPPER_FLAG "-Wl," CACHE INTERNAL "")
-set(CMAKE_ASM_LINKER_WRAPPER_FLAG "-Wl," CACHE INTERNAL "")
-
-set(CMAKE_C_LINKER_WRAPPER_FLAG_SEP "," CACHE INTERNAL "")
-set(CMAKE_CXX_LINKER_WRAPPER_FLAG_SEP "," CACHE INTERNAL "")
-set(CMAKE_ASM_LINKER_WRAPPER_FLAG_SEP "," CACHE INTERNAL "")
-
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -mthumb -mcpu=${VSFHAL_ARCH_NAME}" CACHE INTERNAL "exe link flags")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections --specs=nosys.specs"
+  CACHE INTERNAL "exe link flags")
 
 set(CMAKE_C_COMPILER_WORKS ON)
 set(CMAKE_CXX_COMPILER_WORKS ON)
